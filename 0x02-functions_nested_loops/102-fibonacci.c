dbe346d19783bf8673cd49c307cd1dad3b03fb83
soniaisabel777 <98769533+soniaisabel777@users.noreply.github.com>
2022-02-15 18:38:03 -0500

    modified

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints the Fibonacci secuence
- *   *
- *    * Return: Always 0.
- *     */
+ * * main - Prints the Fibonacci secuence
+ * *
+ * * Return: Always 0.
+ * */
 int main(void)
 {
-	int c;
-	long int f1, f2, fn;
+int c;
+long int f1, f2, fn;
 
-	f1 = 1;
-	f2 = 2;
-	printf("%ld, %ld", f1, f2);
-	for (c = 0; c < 48; c++)
-	{
-		fn = f1 + f2;
-		printf(", %ld", fn);
-		f1 = f2;
-		f2 = fn;
-	}
-	printf("\n");
-	return (0);
+f1 = 1;
+f2 = 2;
+printf("%ld, %ld", f1, f2);
+for (c = 0; c < 48; c++)
+{
+fn = f1 + f2;
+printf(", %ld", fn);
+f1 = f2;
+f2 = fn;
+}
+printf("\n");
+return (0);
 }
