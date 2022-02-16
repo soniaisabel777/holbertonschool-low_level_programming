c825808860cccaece625a7349e90fefa3c8899b4
soniaisabel777 <98769533+soniaisabel777@users.noreply.github.com>
2022-02-15 19:07:11 -0500

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
